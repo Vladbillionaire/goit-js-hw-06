520d2cb535c8987ee08327aa2bbe4232f277bd98
Vladbillionaires <vladislav8174@gmail.com>
2023-08-02 02:54:52 +0300

    Update task 04.js

--- JS/task-04.js
+++ JS/task-04.js
@@ -1,8 +1,9 @@
 const btnDec = document.querySelector('button[data-action="decrement"]');
-const btnInc = document.querySelector('button[data-action="decrement"]');
+const btnInc = document.querySelector('button[data-action="increment"]');
 const valueEl = document.querySelector('#value');
 
 let counterValue = 0;
+
 
 btnDec.addEventListener('click',
     () => {
@@ -10,10 +11,10 @@ btnDec.addEventListener('click',
         valueEl.textContent = counterValue;
     })
 
-    btnInc.addEventListener('click',
+btnInc.addEventListener('click',
     () => {
         counterValue += 1;
         valueEl.textContent = counterValue;
-        })
-    
+    })
+
 console.log(valueEl.textContent);
