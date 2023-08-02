1df335c7b75520995fe4cbe520940276f2735a15
Vladbillionaires <vladislav8174@gmail.com>
2023-08-02 03:18:40 +0300

    Update_task_7
    Correct

--- JS/task-07.js
+++ JS/task-07.js
@@ -1,6 +1,7 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
+textEl.style.fontSize = inputEl.value + "px";
 inputEl.addEventListener('input', (event) => {
     textEl.style.fontSize = event.currentTarget.value + "px";
 })
